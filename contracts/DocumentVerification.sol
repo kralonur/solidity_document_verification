fa05c0c09fb38de63858f0451f03d61fc22e838a
kralonur <kralonur1998@gmail.com>
2022-04-05 16:06:04 +0700

    feat: add function for putting document to verification

--- contracts/DocumentVerification.sol
+++ contracts/DocumentVerification.sol
@@ -23,4 +23,20 @@ contract DocumentVerification {
 
     mapping(bytes32 => Document) private _documents;
     mapping(bytes32 => Sign[]) private _signatures;
+
+    function putDocumentToVerification(
+        bytes32 documentHash,
+        uint128 verificationDeadline,
+        uint128 documentDeadline,
+        VerificationType verificationType,
+        address[] calldata requestedSigners
+    ) external {
+        Document storage document = _documents[documentHash];
+
+        document.verificationCreatedAt = uint128(block.timestamp);
+        document.verificationDeadline = verificationDeadline;
+        document.documentDeadline = documentDeadline;
+        document.verificationType = verificationType;
+        document.requestedSigners = requestedSigners;
+    }
 }
