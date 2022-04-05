bd6b2f34b16c858d5c8d95c7435b7e42cdcaeda4
kralonur <kralonur1998@gmail.com>
2022-04-05 16:13:58 +0700

    feat: add function for revoking the signature

--- contracts/DocumentVerification.sol
+++ contracts/DocumentVerification.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.6;
 import "hardhat/console.sol";
 
 contract DocumentVerification {
+    uint256 public constant INVALID_INDEX = 2**256 - 1;
+
     enum VerificationType {
         INVALID,
         MULTISIG,
@@ -48,5 +50,40 @@ contract DocumentVerification {
         _signatures[documentHash].push(sign);
 
         console.log("Document signed by: %s", msg.sender);
+    }
+
+    function revokeSign(bytes32 documentHash) external {
+        uint256 signerIndex = _getSignedSignerIndex(documentHash, msg.sender);
+
+        console.log("Before pop");
+        for (uint256 i = 0; i < _signatures[documentHash].length; i++) {
+            console.log(_signatures[documentHash][i].signer);
+        }
+
+        // assign latest sign to slot which is going to be delated
+        uint256 latestItemIndex = _signatures[documentHash].length - 1;
+        _signatures[documentHash][signerIndex] = _signatures[documentHash][latestItemIndex];
+
+        // then delete latest sign
+        _signatures[documentHash].pop();
+        console.log("Document sign revoked by: %s", msg.sender);
+
+        console.log("After pop");
+        for (uint256 i = 0; i < _signatures[documentHash].length; i++) {
+            console.log(_signatures[documentHash][i].signer);
+        }
+    }
+
+    function _getSignedSignerIndex(bytes32 documentHash, address signer) private view returns (uint256 index) {
+        Sign[] memory signatures = _signatures[documentHash];
+
+        index = INVALID_INDEX;
+
+        for (uint256 i = 0; i < signatures.length; i++) {
+            if (signatures[i].signer == signer) {
+                index = i;
+                break;
+            }
+        }
     }
 }
