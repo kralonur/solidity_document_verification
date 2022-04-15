c8333d50da80567d13b6cfb9711f38d5338dc092
kralonur <kralonur1998@gmail.com>
2022-04-15 16:55:24 +0700

    refactor: update search library

--- contracts/Search.sol
+++ contracts/Search.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.6;
 library Search {
     uint256 public constant INVALID_INDEX = 2**256 - 1;
 
-    function indexOf(address[] storage self, address value) public view returns (uint256) {
+    function indexOf(address[] storage self, address value) internal view returns (uint256) {
         for (uint256 i = 0; i < self.length; i++) {
             if (self[i] == value) return i;
         }
         return INVALID_INDEX;
     }
 
-    function contains(address[] storage self, address value) public view returns (bool) {
+    function contains(address[] storage self, address value) internal view returns (bool) {
         return indexOf(self, value) != INVALID_INDEX;
     }
 }
